63ca0843310c171bf668950aca457e55fc7feeb5
varnikka0711 <varnikka0711@gmail.com>
2026-03-09 15:35:17 +0530

    uc 10 added

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,17 +1,24 @@
 
-
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
+        String input = "A man a plan a canal Panama";
+        String normalized = input.toLowerCase().replaceAll("[^a-z0-9]", "");
 
-        // Define input string
-        String input = "madam";
+        boolean isPalindrome = true;
 
-        // Call recursive palindrome check
-        boolean result = check(input, 0, input.length() - 1);
+        // 2. Compare characters from both ends (Logic from hint)
+        for (int i = 0; i < normalized.length() / 2; i++) {
+
+            // Compare symmetric characters
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
 
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + result);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 
     private static boolean check(String s, int start, int end) {
