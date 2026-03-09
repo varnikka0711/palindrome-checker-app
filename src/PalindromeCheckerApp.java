8394fee6bfcd46fe2e58ff8fe4c2a2dddb08c3af
varnikka0711 <varnikka0711@gmail.com>
2026-03-09 15:23:54 +0530

    add uc9

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,30 @@
+
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Define input string
+        String input = "madam";
+
+        // Call recursive palindrome check
+        boolean result = check(input, 0, input.length() - 1);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+    }
+
+    private static boolean check(String s, int start, int end) {
+
+        if (start >= end) {
+            return true;}
+
+        // If characters don't match
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call moving inward
+        return check(s, start + 1, end - 1);
+    }
+}
